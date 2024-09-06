83c969f9d577335c4caef55f017c789e10f6e86b
Békési Bálint <bekesi.balint@burg.lo.hu>
2024-09-06 10:36:46 +0200

    legelső commit

---
+++ Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Brickstore
+{
+    internal class Item
+    {
+        public string ItemID { get; set; }
+        public string ItemTypeID { get; set; }
+        public string ColorID { get; set; }
+        public string ItemName { get; set; }
+        public string ItemTypeName { get; set; }
+        public string ColorName { get; set; }
+        public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string Status { get; set; }
+        public int Qty { get; set; }
+        public decimal Price { get; set; }
+        public string Condition { get; set; }
+        public int OrigQty { get; set; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Windows;
+
+namespace WPF_Brickstore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Item> items = new List<Item>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ReadXamlFile();
+        }
+
+        private void ReadXamlFile()
+        {
+            
+            XDocument xaml = XDocument.Load("brickstore_parts_7288-1-mobile-police-unit.bsx");
+
+            foreach (var elem in xaml.Descendants("Item"))
+            {
+                Item item = new Item
+                {
+                    ItemID = elem.Element("ItemID").Value,
+                    ItemTypeID = elem.Element("ItemTypeID").Value,
+                    ColorID = elem.Element("ColorID").Value,
+                    ItemName = elem.Element("ItemName").Value,
+                    ItemTypeName = elem.Element("ItemTypeName").Value,
+                    ColorName = elem.Element("ColorName").Value,
+                    CategoryID = elem.Element("CategoryID").Value,
+                    CategoryName = elem.Element("CategoryName").Value,
+                    Status = elem.Element("Status").Value,
+                    Qty = int.Parse(elem.Element("Qty").Value),
+                    Price = decimal.Parse(elem.Element("Price").Value),
+                    Condition = elem.Element("Condition").Value,
+                    OrigQty = int.Parse(elem.Element("OrigQty").Value)
+                };
+
+                items.Add(item);
+            }
+
+         
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.ItemName);
+            }
+        }
+    }
+}
